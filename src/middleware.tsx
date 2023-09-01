c3587a948c25522912c741f01d812cec27db0cb6
Marc Cavazos <marcavazos19@icloud.com>
2023-09-01 17:26:22 -0500

    clerk set up

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,10 +1,9 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default authMiddleware({});
- 
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+import { withClerkMiddleware } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+
+export default withClerkMiddleware(() => {
+  console.log('middleware running');
+  return NextResponse.next();
+});
+
+export const config = { matcher: '/((?!_next/image|_next/static|favicon.ico).*)',};
